4810c8fa134128887a1b0aa2b4f82b07e1c0d1e0
Robin Damsgaard Larsen <rdalarsen@gmail.com>
2018-04-07 20:57:08 +0200

    initial commit

---
+++ app/src/main/java/me/worric/bakingtime/ui/main/MainActivity.java
@@ -0,0 +1,15 @@
+package me.worric.bakingtime.ui.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import me.worric.bakingtime.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
