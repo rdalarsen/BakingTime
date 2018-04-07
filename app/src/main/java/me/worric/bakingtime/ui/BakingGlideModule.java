1d19f130c9948815c8fea1f45552d6ed92909f14
Robin Damsgaard Larsen <rdalarsen@gmail.com>
2018-04-07 22:15:28 +0200

    setup Glide

---
+++ app/src/main/java/me/worric/bakingtime/ui/BakingGlideModule.java
@@ -0,0 +1,23 @@
+package me.worric.bakingtime.ui;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+import com.bumptech.glide.GlideBuilder;
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.module.AppGlideModule;
+import com.bumptech.glide.request.RequestOptions;
+
+@GlideModule
+public class BakingGlideModule extends AppGlideModule {
+
+    @Override
+    public void applyOptions(@NonNull Context context, @NonNull GlideBuilder builder) {
+        builder.setDefaultRequestOptions(
+                new RequestOptions()
+                        .diskCacheStrategy(DiskCacheStrategy.ALL)
+        );
+    }
+
+}
