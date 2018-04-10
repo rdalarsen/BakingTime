436a0d476cff2f54f8c1e640afd63720ff002d94
Robin Damsgaard Larsen <rdalarsen@gmail.com>
2018-04-10 21:34:16 +0200

    removed multi-view recyclerview, add util method to correct missing step

--- app/src/main/java/me/worric/bakingtime/ui/detail/StepsAdapter.java
+++ app/src/main/java/me/worric/bakingtime/ui/detail/StepsAdapter.java
@@ -1,7 +1,6 @@
 package me.worric.bakingtime.ui.detail;
 
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,7 +15,6 @@ import butterknife.ButterKnife;
 import me.worric.bakingtime.R;
 import me.worric.bakingtime.data.models.Step;
 import me.worric.bakingtime.ui.GlideApp;
-import timber.log.Timber;
 
 public class StepsAdapter extends RecyclerView.Adapter<StepsAdapter.StepsViewHolder> {
 
@@ -35,46 +33,21 @@ public class StepsAdapter extends RecyclerView.Adapter<StepsAdapter.StepsViewHol
     @NonNull
     @Override
     public StepsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View v;
-        switch (viewType) {
-            case 0:
-                v = LayoutInflater.from(parent.getContext())
-                        .inflate(R.layout.list_item_master, parent, false);
-                break;
-            case 1:
-                v = LayoutInflater.from(parent.getContext())
-                        .inflate(R.layout.list_item_master_ingredients, parent, false);
-                break;
-            default:
-                throw new IllegalArgumentException("Unknown view type: " + viewType);
-        }
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.list_item_master, parent, false);
         return new StepsViewHolder(v);
     }
 
     @Override
     public void onBindViewHolder(@NonNull StepsViewHolder holder, int position) {
-        switch (holder.getItemViewType()) {
-            case 0:
-                final Step step = mSteps.get(position);
-                holder.mShortDesc.setText(step.getShortDescription());
-                String stepText = "Step " + position;
-                holder.mStepNumber.setText(stepText);
-                GlideApp.with(holder.itemView.getContext())
-                        .load(step.getThumbnailURL())
-                        .into(holder.mThumbnail);
-                holder.itemView.setOnClickListener(v -> mListener.onStepClick(step));
-                break;
-            case 1:
-                holder.itemView.setOnClickListener(v -> Timber.d("Ingredients clicked"));
-                break;
-            default:
-                throw new IllegalArgumentException("Unknown view type: " + holder.getItemViewType());
-        }
-    }
-
-    @Override
-    public int getItemViewType(int position) {
-        return position == 0 ? 1 : 0;
+        final Step step = mSteps.get(position);
+        holder.mShortDesc.setText(step.getShortDescription());
+        String stepText = "Step " + position;
+        holder.mStepNumber.setText(stepText);
+        GlideApp.with(holder.itemView.getContext())
+                .load(step.getThumbnailURL())
+                .into(holder.mThumbnail);
+        holder.itemView.setOnClickListener(v -> mListener.onStepClick(step));
     }
 
     @Override
@@ -84,11 +57,11 @@ public class StepsAdapter extends RecyclerView.Adapter<StepsAdapter.StepsViewHol
 
     static class StepsViewHolder extends RecyclerView.ViewHolder {
 
-        @Nullable @BindView(R.id.tv_master_step_short_desc)
+        @BindView(R.id.tv_master_step_short_desc)
         TextView mShortDesc;
-        @Nullable @BindView(R.id.iv_master_thumb)
+        @BindView(R.id.iv_master_thumb)
         ImageView mThumbnail;
-        @Nullable @BindView(R.id.tv_master_step_number)
+        @BindView(R.id.tv_master_step_number)
         TextView mStepNumber;
 
         public StepsViewHolder(View itemView) {
--- app/src/main/java/me/worric/bakingtime/ui/util/UiUtils.java
+++ app/src/main/java/me/worric/bakingtime/ui/util/UiUtils.java
@@ -3,9 +3,34 @@ package me.worric.bakingtime.ui.util;
 import android.content.Context;
 import android.util.DisplayMetrics;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import me.worric.bakingtime.data.models.Step;
+
 public final class UiUtils {
 
     private UiUtils() {
+    }
+
+    public static void correctMissingSteps(List<Step> steps) {
+        if (steps == null) return;
+
+        List<Long> mMissingSteps = new ArrayList<>(1);
+        for(int i = 0; i < steps.size(); i++) {
+            Step step = steps.get(i);
+            if (step.getId() != (i + mMissingSteps.size())) {
+                mMissingSteps.add((long) i);
+            }
+        }
+
+        if (!mMissingSteps.isEmpty()) {
+            for (long i : mMissingSteps) {
+                Step step = new Step();
+                step.setId(-1L);
+                steps.add((int) i, step);
+            }
+        }
     }
 
     public static int calculateSpanCount(Context context) {
